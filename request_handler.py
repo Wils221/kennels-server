2cee1d6123e888ae2693b8e00be29f1f31f4066d
Wil Hammond <wil.hammond@gmail.com>
2023-01-24 14:42:22 -0600

    through book2 ch3

--- request_handler.py
+++ request_handler.py
@@ -1,10 +1,10 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
+from views import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_animal_by_location, get_animal_by_status
 from views import get_all_locations, get_single_location, create_location, delete_location, update_location
-from views import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
-from views import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
-
+from views import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee, get_employee_by_location
+from views import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer, get_customers_by_email, get_customers_by_name
+from urllib.parse import urlparse, parse_qs
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -17,67 +17,75 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It gives a description of the class or function
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
     """
-
+    # replace the parse_url function in the class
     def parse_url(self, path):
-        # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
-        # at index 2.
-        path_params = path.split("/")
+        """Parse the url into the resource and id"""
+        parsed_url = urlparse(path)
+        path_params = parsed_url.path.split('/')  # ['', 'animals', 1]
         resource = path_params[1]
-        id = None
 
-        # Try to get the item at index 2
+        if parsed_url.query:
+            query = parse_qs(parsed_url.query)
+            return (resource, query)
+
+        pk = None
         try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
-            id = int(path_params[2])
-        except IndexError:
-            pass  # No route parameter exists: /animals
-        except ValueError:
-            pass  # Request had trailing slash: /animals/
-
-        return (resource, id)  # This is a tuple
-    # Here's a class function
+            pk = int(path_params[2])
+        except (IndexError, ValueError):
+            pass
+        return (resource, pk)
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
     def do_GET(self):
-        self._set_headers(200)
-        response = {}  # Default response
+            self._set_headers(200)
 
-        # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path)
+            response = {}
 
-        if resource == "animals":
-            if id is not None:
-                response = get_single_animal(id)
+        # Parse URL and store entire tuple in a variable
+            parsed = self.parse_url(self.path)
 
-            else:
-                response = get_all_animals()
+        # If the path does not include a query parameter, continue with the original if block
+            if '?' not in self.path:
+                ( resource, id ) = parsed
 
-        elif resource == "locations":
-            if id is not None:
-                response = get_single_location(id)
+                if resource == "animals":
+                    if id is not None:
+                        response = get_single_animal(id)
+                    else:
+                        response = get_all_animals()
+                elif resource == "customers":
+                    if id is not None:
+                        response = get_single_customer(id)
+                    else:
+                        response = get_all_customers()
+                elif resource == "locations":
+                    if id is not None:
+                        response = get_single_location(id)
+                    else:
+                        response = get_all_locations()
+                elif resource == "employees":
+                    if id is not None:
+                        response = get_single_employee(id)
+                    else:
+                        response = get_all_employees()        
 
-            else:
-                response = get_all_locations()
+            else: # There is a ? in the path, run the query param functions
+                (resource, query) = parsed
 
-        elif resource == "employees":
-            if id is not None:
-                response = get_single_employee(id)
+            # see if the query dictionary has an email key
+                if query.get('email') and resource == 'customers':
+                    response = get_customers_by_email(query['email'][0])
+                elif query.get('location_id') and resource == 'animals':
+                    response = get_animal_by_location(query['location_id'][0])
+                elif query.get('location_id') and resource == 'employees':
+                    response = get_employee_by_location(query['location_id'][0])
+                elif query.get('status') and resource == 'animals':
+                    response = get_animal_by_status(query['status'][0])
+                elif query.get('name') and resource == 'customers':
+                    response = get_customers_by_name(query['name'][0])
 
-            else:
-                response = get_all_employees()
-
-        elif resource == "customers":
-            if id is not None:
-                response = get_single_customer(id)
-
-            else:
-                response = get_all_customers()
-
-        self.wfile.write(json.dumps(response).encode())
+            self.wfile.write(json.dumps(response).encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
--- views/customer_requests.py
+++ views/customer_requests.py
@@ -1,3 +1,8 @@
+import sqlite3
+import json
+from models import Customer
+
+
 CUSTOMERS = [
     {
         "id": 1,
@@ -5,22 +10,69 @@ CUSTOMERS = [
     }
 ]
 def get_all_customers():
-    return CUSTOMERS
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.sqlite3") as conn:
 
-    # Function with a single parameter
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address,
+            a.email,
+            a.password
+        FROM customer a
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        customers = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            customer = Customer(row['id'], row['name'], row['address'], row['email'], row['password'])
+
+            customers.append(customer.__dict__)
+
+    return customers
+
 def get_single_customer(id):
-    # Variable to hold the found animal, if it exists
-    requested_customer = None
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    # Iterate the ANIMALS list above. Very similar to the
-    # for..of loops you used in JavaScript.
-    for customer in CUSTOMERS:
-        # Dictionaries in Python use [] notation to find a key
-        # instead of the dot notation that JavaScript used.
-        if customer["id"] == id:
-            requested_customer = customer
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address,
+            a.email,
+            a.password
+        FROM customer a
+        WHERE a.id = ?
+        """, ( id, ))
 
-    return requested_customer
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an animal instance from the current row
+        customer = Customer(data['id'], data['name'], data['address'], data['email'], data['password'])
+
+        return customer.__dict__
+
 
 def create_customer(customer):
     # Get the id value of the last animal in the list
@@ -38,6 +90,7 @@ def create_customer(customer):
     # Return the dictionary with `id` property added
     return customer
 
+
 def delete_customer(id):
     # Initial -1 value for animal index, in case one isn't found
     customer_index = -1
@@ -53,6 +106,7 @@ def delete_customer(id):
     if customer_index >= 0:
         CUSTOMERS.pop(customer_index)
 
+
 def update_customer(id, new_customer):
     # Iterate the ANIMALS list, but use enumerate() so that
     # you can access the index value of each item.
@@ -61,3 +115,57 @@ def update_customer(id, new_customer):
             # Found the animal. Update the value.
             CUSTOMERS[index] = new_customer
             break
+
+def get_customers_by_email(email):
+
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        select
+            c.id,
+            c.name,
+            c.address,
+            c.email,
+            c.password
+        from Customer c
+        WHERE c.email = ?
+        """, ( email, ))
+
+        customers = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            customer = Customer(row['id'], row['name'], row['address'], row['email'] , row['password'])
+            customers.append(customer.__dict__)
+
+    return customers
+
+def get_customers_by_name(name):
+
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        select
+            c.id,
+            c.name,
+            c.address,
+            c.email,
+            c.password
+        from Customer c
+        WHERE c.name = ?
+        """, ( name, ))
+
+        customers = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            customer = Customer(row['id'], row['name'], row['address'], row['email'] , row['password'])
+            customers.append(customer.__dict__)
+
+    return customers
